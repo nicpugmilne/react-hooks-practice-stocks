89df129d826850d4523c4354f72ce4e9a84bcf05
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-15 15:27:35 -0600

    commit tree 6dc1659bcc45993fe4462c6ebf55e0970de9f624

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState("Alphabetically");
+  const [filterBy, setFilterBy] = useState("Tech");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((r) => r.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  function handleStockClick(stockClicked) {
+    const stockInPortfolio = portfolio.find(
+      (stock) => stock.id === stockClicked.id
+    );
+    if (!stockInPortfolio) {
+      addToPortfolio(stockClicked);
+    } else {
+      removeFromPortfolio(stockClicked);
+    }
+  }
+
+  function addToPortfolio(stockToAdd) {
+    setPortfolio([stockToAdd, ...portfolio]);
+  }
+
+  function removeFromPortfolio(stockToRemove) {
+    console.log(stockToRemove);
+    const updatedPortfolio = portfolio.filter(
+      (stock) => stock.id !== stockToRemove.id
+    );
+    setPortfolio(updatedPortfolio);
+  }
+
+  const sortedStocks = [...stocks].sort((stock1, stock2) => {
+    if (sortBy === "Alphabetically") {
+      return stock1.name.localeCompare(stock2.name);
+    } else {
+      return stock1.price - stock2.price;
+    }
+  });
+
+  const filteredStocks = sortedStocks.filter(
+    (stock) => stock.type === filterBy
+  );
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        sortBy={sortBy}
+        onChangeSort={setSortBy}
+        filterBy={filterBy}
+        onChangeFilter={setFilterBy}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} onClick={handleStockClick} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolio} onClick={handleStockClick} />
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ stocks, onClick }) {
+  const stocksItems = stocks.map((stock) => {
+    return <Stock key={stock.id} stock={stock} onClick={onClick} />;
+  });
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {stocksItems}
     </div>
   );
 }
